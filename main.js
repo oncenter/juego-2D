219b4b63c1562d3e02b23e9aa3a1f3d5ebb04f3a
oncenter <tatolucero07@gmail.com>
2025-07-17 20:17:04 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
 const config = {
   type: Phaser.AUTO,
-  width: 800,
+  width: 600,
   height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 500 },
-      debug: false
-    }
-  },
+  backgroundColor: '#1d1d1d',
   scene: {
     preload: preload,
     create: create,
@@ -16,87 +10,97 @@ const config = {
   }
 };
 
-let player;
-let platforms;
-
 const game = new Phaser.Game(config);
 
-function preload() {
-  // Cargar imágenes
-  this.load.image('sky', 'https://labs.phaser.io/assets/skies/sky4.png');
-  this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
-  this.load.spritesheet('dude',
-    'https://labs.phaser.io/assets/sprites/dude.png',
-    { frameWidth: 32, frameHeight: 48 }
-  );
-}
+let snake;
+let food;
+let direction = 'RIGHT';
+let nextDirection = 'RIGHT';
+let lastMoveTime = 0;
+let moveInterval = 150;
+let gridSize = 20;
+let score = 0;
+let scoreText;
+
+function preload() {}
 
 function create() {
-  // Fondo
-  this.add.image(400, 300, 'sky');
+  snake = [];
+  snake.push(this.add.rectangle(100, 100, gridSize, gridSize, 0x00ff00).setOrigin(0));
 
-  // Plataformas estáticas
-  platforms = this.physics.add.staticGroup();
+  placeFood.call(this);
 
-  // Suelo
-  platforms.create(400, 584, 'ground').setScale(2).refreshBody();
-
-  // Plataformas flotantes
-  platforms.create(600, 450, 'ground');
-  platforms.create(50, 350, 'ground');
-  platforms.create(750, 300, 'ground');
-
-  // Jugador
-  player = this.physics.add.sprite(100, 450, 'dude');
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  // Animaciones del jugador
-  this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
+  // Teclado
+  this.input.keyboard.on('keydown', event => {
+    switch (event.key.toUpperCase()) {
+      case 'W':
+        if (direction !== 'DOWN') nextDirection = 'UP';
+        break;
+      case 'S':
+        if (direction !== 'UP') nextDirection = 'DOWN';
+        break;
+      case 'A':
+        if (direction !== 'RIGHT') nextDirection = 'LEFT';
+        break;
+      case 'D':
+        if (direction !== 'LEFT') nextDirection = 'RIGHT';
+        break;
+    }
   });
 
-  this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
-  });
-
-  this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  // Colisión jugador - plataformas
-  this.physics.add.collider(player, platforms);
-
-  // Control de teclado con W,A,S,D
-  this.wKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  this.aKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  this.sKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-  this.dKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  scoreText = this.add.text(10, 10, 'Puntaje: 0', { font: '20px Arial', fill: '#ffffff' });
 }
 
-function update() {
-  if (this.aKey.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play('left', true);
-  }
-  else if (this.dKey.isDown) {
-    player.setVelocityX(160);
-    player.anims.play('right', true);
-  }
-  else {
-    player.setVelocityX(0);
-    player.anims.play('turn');
-  }
-
-  if (this.wKey.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+function update(time) {
+  if (time >= lastMoveTime + moveInterval) {
+    lastMoveTime = time;
+    moveSnake.call(this);
   }
 }
+
+function moveSnake() {
+  direction = nextDirection;
+
+  const head = snake[0];
+  let newX = head.x;
+  let newY = head.y;
+
+  if (direction === 'LEFT') newX -= gridSize;
+  else if (direction === 'RIGHT') newX += gridSize;
+  else if (direction === 'UP') newY -= gridSize;
+  else if (direction === 'DOWN') newY += gridSize;
+
+  // Colisión con paredes
+  if (newX < 0 || newX >= config.width || newY < 0 || newY >= config.height) {
+    gameOver.call(this);
+    return;
+  }
+
+  // Colisión con sí mismo
+  for (let i = 0; i < snake.length; i++) {
+    if (snake[i].x === newX && snake[i].y === newY) {
+      gameOver.call(this);
+      return;
+    }
+  }
+
+  // Mover la serpiente
+  const newHead = this.add.rectangle(newX, newY, gridSize, gridSize, 0x00ff00).setOrigin(0);
+  snake.unshift(newHead);
+
+  // Comió la comida
+  if (newX === food.x && newY === food.y) {
+    food.destroy();
+    placeFood.call(this);
+    score += 10;
+    scoreText.setText('Puntaje: ' + score);
+  } else {
+    const tail = snake.pop();
+    tail.destroy();
+  }
+}
+
+function placeFood() {
+  const foodX = Phaser.Math.Between(0, (config.width / gridSize) - 1) * gridSize;
+  const foodY = Phaser.Math.Between(0, (config.height / gridSize) - 1) * gridSize;
+  food = this.add.rec
